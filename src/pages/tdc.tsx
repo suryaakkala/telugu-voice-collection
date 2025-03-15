614c7e990269d3622c3ebc753fe809ea93ba71ef
SURYA AKKALA <141207082+suryaakkala@users.noreply.github.com>
2025-03-15 14:47:40 +0530

    Update tdc.tsx

--- src/pages/tdc.tsx
+++ src/pages/tdc.tsx
@@ -1,9 +1,9 @@
 import TDC from "@/components/tdc";
-import "@/styles/globals.css";
+// import "@/styles/globals.css";
 export default function TDCPage() {
     return (
         <>
-      <style jsx global>{`
+      <style jsx>{`
         body {
           margin: 0; /* Remove default margin */
           padding: 0; /* Remove default padding */
