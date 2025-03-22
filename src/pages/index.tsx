47458285f292088e3f205e0d3eaca0c6b55dbc63
suryaakkala <ayrus2222@gmail.com>
2025-03-22 15:08:54 +0530

    fc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,35 +3,9 @@ import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
-  title: "Telugu Chat Bot",
-  description: "Experience the power of conversing in Telugu with our advanced AI chatbot.",
+  title: "Telugu Voice Collection",
+  description: "Thankyou For your Contribution.",
 };
-
-// const Header: React.FC = () => {
-//   return (
-//     <header
-//       style={{
-//         backgroundColor: "#DFF6FF",
-//         boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-//         display: "flex",
-//         flexDirection: "row",
-//         width: "100%",
-//         alignItems: "center",
-//         height: "40px",
-//         padding: "4px 4px",
-//         justifyContent: "space-between",
-//         position: "fixed",
-//         top: 0,
-//         left: 0,
-//         zIndex: 1000,
-//       }}
-//     >
-//       <img src="/klu.png" alt="Left Logo" style={{ height: "40px" }} />
-//       <img src="/klug.png" alt="Right Logo" style={{ height: "40px" }} />
-//     </header>
-//   );
-// };
-
 
 export default function Home() {
   return (
